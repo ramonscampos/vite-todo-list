152fc698203081b396f86b9792c5ecdec26a0033
Ramon Campos <ramonscampos@gmail.com>
2022-06-21 22:15:38 -0300

    fix: task description color

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -9,13 +9,18 @@ interface TaskCardProps {
     onDelete: (task: Task) => void;
 }
 
+
 export function TaskCard({ task, onToggleDone, onDelete }: TaskCardProps) {
+    const getDescriptionClass = () => {
+        return task.done ? 'text-gray-300 line-through' : 'text-gray-100';
+    }
+
     return (
         <div className="border border-gray-400 bg-gray-500 rounded-lg p-4 gap-3 shadow-md shadow-[#0000000f] flex">
             <button type="button" onClick={() => onToggleDone(task)}>
                 {task.done ? <RadioFilled /> : <Radio />}
             </button>
-            <span className={`text-sm text-gray-${task.done ? '300' : '100'} flex-1 ${task.done ? 'line-through' : ''}`}>{task.description}</span>
+            <span className={`text-sm flex-1 ${getDescriptionClass()}`}>{task.description}</span>
             <button type="button" onClick={() => onDelete(task)}>
                 <Trash />
             </button>
